4d52e04a5bf2a4bb3da064c6c06f8cdce305db50
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-02-09 17:51:18 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card1 from './com/card-1'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Card1/>
+    </div>
+  )
+}
+
+export default App
